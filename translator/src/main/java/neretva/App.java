264dc904cf85df6eb20d560bebb7e32237ca25f3
Hamza <hamzamak@ntnu.no>
2024-03-08 22:06:41 +0100

    Changes to be committed: new file:   translator/pom.xml new file:   translator/src/main/java/neretva/App.java new file:   translator/src/test/java/neretva/AppTest.java

---
+++ translator/src/main/java/neretva/App.java
@@ -0,0 +1,13 @@
+package neretva;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
