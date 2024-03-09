759b1b4064fb8d52f3ad4a48cea46d8d5799eb72
Hamza <hamzamak@ntnu.no>
2024-03-09 12:49:32 +0100

    first functional beta GUI for translation program modified:   GUI/javafx-archetype-fxml/src/main/java/org/openjfx/PrimaryController.java modified:   GUI/javafx-archetype-fxml/src/main/resources/org/openjfx/primary.fxml modified:   GUI/javafx-archetype-fxml/target/classes/org/openjfx/primary.fxml

--- GUI/javafx-archetype-fxml/src/main/java/org/openjfx/PrimaryController.java
+++ GUI/javafx-archetype-fxml/src/main/java/org/openjfx/PrimaryController.java
@@ -2,11 +2,34 @@ package org.openjfx;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private CheckBox bokmålBoolean;
+
+    @FXML
+    private TextArea inputText;
+
+    @FXML
+    private Label outputLabel;
+
+    //@FXML
+    //private void switchToSecondary() throws IOException {
+    //    App.setRoot("secondary");
+    //}
+
+    @FXML
+    public void translateText() {
+        String text = inputText.getText();
+        boolean bokmålStatus = bokmålBoolean.isSelected();
+        Translation translate = new Translation(text, bokmålStatus);
+        
+        String translatedOutput = translate.getTranslation();
+        outputLabel.setText(translatedOutput);
+
     }
 }
