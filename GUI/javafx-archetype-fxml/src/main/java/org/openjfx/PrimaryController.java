1f1c0519585275a10d426075d9730639466c6b7a
Hamza <hamzamak@ntnu.no>
2024-03-10 19:47:12 +0100

    Added button that copies output to clipboard modified:   GUI/javafx-archetype-fxml/src/main/java/org/openjfx/PrimaryController.java modified:   GUI/javafx-archetype-fxml/src/main/resources/org/openjfx/primary.fxml modified:   GUI/javafx-archetype-fxml/target/classes/org/openjfx/primary.fxml

--- GUI/javafx-archetype-fxml/src/main/java/org/openjfx/PrimaryController.java
+++ GUI/javafx-archetype-fxml/src/main/java/org/openjfx/PrimaryController.java
@@ -2,10 +2,15 @@ package org.openjfx;
 
 import java.io.IOException;
 
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.util.Duration;
 
 public class PrimaryController {
 
@@ -19,8 +24,27 @@ public class PrimaryController {
     private Label outputLabel;
 
     @FXML
+    private Button copyButton;
+
+    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+
+    }
+
+    @FXML
+    public void copyToClipboard() {
+        Clipboard clipboard = Clipboard.getSystemClipboard();
+        ClipboardContent copiedText = new ClipboardContent();
+
+        copiedText.putString(outputLabel.getText());
+        clipboard.setContent(copiedText);
+
+        copyButton.setText("Kopiert!");
+
+            PauseTransition delay = new PauseTransition(Duration.seconds(2)); //skaper en pause på 2 sek
+            delay.setOnFinished(actionEvent -> copyButton.setText("Kopier")); //setter teksten til knappen tilbake etter pausen
+            delay.play();
 
     }
 
