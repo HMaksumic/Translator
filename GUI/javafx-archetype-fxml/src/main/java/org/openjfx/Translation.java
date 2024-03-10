b063b678e0c1dcfacdf679e8ef5ec6bc3d558af3
Hamza <hamzamak@ntnu.no>
2024-03-10 09:56:38 +0100

    added functionality for special characters. modified:   GUI/javafx-archetype-fxml/src/main/java/org/openjfx/Translation.java

--- GUI/javafx-archetype-fxml/src/main/java/org/openjfx/Translation.java
+++ GUI/javafx-archetype-fxml/src/main/java/org/openjfx/Translation.java
@@ -1,6 +1,5 @@
 package org.openjfx;
 
-//hvis noen ser det her, så gjør jeg dette pga jeg aner ikke hvordan jeg importerer translationDemo hit :(((;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,7 +13,8 @@ public class Translation {
                                                             "penger","grønn","rød","svart","hvit","trøtt","sulten","trist","dårlig","kjedelig","lavt","treg","fremdeles","øynene","hver","vanlig","gammel","sjelden","vanskelig","høy","lav",
                                                             "engang", "ubruklig", "beboer", "kjærlighet", "bekreftelse", "sannhet", "drøm", "spise", "underlig", "syk", "hvis", "hode" ,"morgen", "følelse", "følelser", "følelsene",
                                                             "hjem", "leke", "anderledes", "fornøyd", "velge", "hensyn", "kjedelig", "tenke", "nyhet", "hemmelighet", "hemmeligheter", "hemmelighetene", "selge", "tregere", "tregest", "øyne", "flere",
-                                                            "rettferdighet", "lærer", "lærere", "lærerene", "valg", "valget", "valgene"
+                                                            "rettferdighet", "lærer", "lærere", "lærerene", "valg", "valget", "valgene","boligen","boliger","boligene",
+                                                            "venner", "vennene"
 
                                                             
                                                             
@@ -25,11 +25,12 @@ public class Translation {
                                                             "desse", "gjer", "fekk", "veg", "stad", "framleis", "finst", "seier", "eg", "ikkje", "ho", "frå",   "då", "honom", "no", "noko",
                                                             "dykk", "ho", "skjølv", "vore", "gjekk", "meir", "kva", "kor", "sidan", "heile", "gong", "saman", "skulen", "ska", "synast",
                                                             "elskar", "noreg", "gut", "gløyme", "gløymer", "gløymt", "skriv", "skrive", "skreiv", "hata", "kvifor", "veke", "nærleiken", "korkje", "høyre",
-                                                            "mjølk", "bilar", "kastar", "kasta", "kasta", "nyleg", "ærleg", "ovleg", "boleg", "mogleg", "roleg", "korleis", "frykteleg", "høgleg", "lovleg", "synleg", "kunstleg",
+                                                            "mjølk", "bilar", "kastar", "kasta", "kasta", "nyleg", "ærleg", "ovleg", "bustad", "mogleg", "roleg", "korleis", "frykteleg", "høgleg", "lovleg", "synleg", "kunstleg",
                                                             "pengar","grøn","raud","svart","kvit","trøytt","svolten","trist","dårleg","kjedeleg","lågt","treig","framleis","augo","kvar","vanleg","gamal","sjeldan","vanskeleg","høg","låg",
                                                             "eingong", "ubrukeleg", "bebuar", "kjærleik", "stadfesting", "sanning", "draum","ete", "underleg", "sjuk", "viss", "hovud", "morgon", "kjensla", "kjensler", "kjenslene",
                                                             "heim", "leike", "annleis", "nøgd", "velje", "omsyn", "keisam", "tenkje", "nyheit", "løyndom", "løyndommar", "løyndommane", "selje", "treigare", "treigast", "augo", "fleire",
-                                                            "rettferd", "lærar", "lærarar", "lærarane", "val", "valet", "vala"
+                                                            "rettferd", "lærar", "lærarar", "lærarane", "val", "valet", "vala", "bustaden","bustader", "bustadene",
+                                                            "vennar", "vennane"
                 
 
 
@@ -43,14 +44,13 @@ public class Translation {
     private List<String> translatedText = new ArrayList<>();
     private String translatedString = "";
     
-    boolean bokmålStatus;
-
+    private boolean bokmålStatus;
     
     Translation(String text, boolean bokmålStatus) {
         this.bokmålStatus = bokmålStatus;
         text.toLowerCase();
 
-        String[] splitText = text.split(" ");
+        String[] splitText = text.split(" ");  //"jeg går til skolen!" ----> ["jeg", "går", "til", "skolen!"]
         for (String word : splitText) {
             this.dividedText.add(word);
         }
@@ -61,11 +61,22 @@ public class Translation {
                 //har et bokmål ord her og leter etter det samme ordet i bokmål ordboken;
                 boolean foundWord = false; //hvis ordet ikke blir funnet i ordboken skal det orginale ordet brukes
                 for (int j = 0; j < bokmålOrdbok.size(); j++) {
-                    if (bokmålOrdbok.get(j).matches(dividedText.get(i))) {
-                        //siden ordbøkene har 1:1 forhold tar man indexen til det matchende bokmål ordet og bruker den med nynorsk ordboken.
-                        this.translatedText.add(nynorskOrdbok.get(j));
-                        foundWord = true;
 
+                    if (dividedText.get(i).matches(bokmålOrdbok.get(j) + "([!?,;'\\.]*)")) {         
+                        //siden ordbøkene har 1:1 forhold tar man indexen til det matchende bokmål ordet og bruker den med nynorsk ordboken.
+                        String theWord = dividedText.get(i);
+                        int lastLetter = theWord.length() - 1; char specialChar = theWord.charAt(lastLetter);
+                        String specialString = String.valueOf(specialChar); //henter ut siste tegn fra matchende ord som streng
+
+                        if (specialString.matches("([!?,;'\\.]*)")) { 
+                            this.translatedText.add(nynorskOrdbok.get(j) + specialString);
+
+                        } 
+                        else { 
+                            this.translatedText.add(nynorskOrdbok.get(j));
+
+                        }
+                        foundWord = true;
                     }
                 } 
                 if (!foundWord) { //orginalt ord brukes isteden
@@ -78,9 +89,21 @@ public class Translation {
                 //har et nynorsk ord her og leter etter det samme ordet i nynorsk ordboken;
                 boolean foundWord = false; //hvis ordet ikke blir funnet i ordboken skal det orginale ordet brukes
                 for (int j = 0; j < nynorskOrdbok.size(); j++) {
-                    if (nynorskOrdbok.get(j).matches(dividedText.get(i))) {
+
+                    if (dividedText.get(i).matches(nynorskOrdbok.get(j) + "([!?,;'\\.]*)")) {
                         //siden ordbøkene har 1:1 forhold tar man indexen til det matchende bokmål ordet og bruker den med nynorsk ordboken.
-                        this.translatedText.add(bokmålOrdbok.get(j));
+                        String theWord = dividedText.get(i);
+                        int lastLetter = theWord.length() - 1; char specialChar = theWord.charAt(lastLetter); 
+                        String specialString = String.valueOf(specialChar);
+
+                        if (specialString.matches("([!?,;'\\.]*)")) {
+                            this.translatedText.add(bokmålOrdbok.get(j) + specialString);
+
+                        } 
+                        else { 
+                            this.translatedText.add(bokmålOrdbok.get(j));
+
+                        }
                         foundWord = true;
 
                     }
@@ -90,7 +113,6 @@ public class Translation {
                 }
             }
         }
-
         if (translatedText.size() > 0) {
             for (String word : translatedText) {
                 this.translatedString += " " + word;
@@ -98,17 +120,17 @@ public class Translation {
             }
         }
     }
-
     public String getTranslation() {
         System.out.println(this.translatedString);
         return this.translatedString;
     }
+    public boolean getBokmålStatus() {
+        return this.bokmålStatus;
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //test kode
 
-
-        
-        Translation demo = new Translation("jeg heter hode selv hvis", true);
+        Translation demo = new Translation("eg!", false);
         demo.getTranslation();
 
     }
