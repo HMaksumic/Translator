8542fd1130a53fe989da73da95fa57884f3f7d3a
Hamza <hamzamak@ntnu.no>
2024-03-19 11:47:48 +0100

    vocab update     modified:   GUI/javafx-archetype-fxml/src/main/java/org/openjfx/Translation.java 	new file:   GUI/javafx-archetype-fxml/src/main/java/org/openjfx/ord.txt 	new file:   GUI/javafx-archetype-fxml/target/classes/org/openjfx/ord.txt 	modified:   GUI/javafx-archetype-fxml/target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst

--- GUI/javafx-archetype-fxml/src/main/java/org/openjfx/Translation.java
+++ GUI/javafx-archetype-fxml/src/main/java/org/openjfx/Translation.java
@@ -19,7 +19,28 @@ public class Translation {
                                                             "politianmeldelsen","politianmeldelser","politianmeldelsene","bokanmeldelsen","bokanmeldelser","bokanmeldelsene",
                                                             "anmodning", "anmodningsvedtak", "annerledes", "ansatt", "anselig", "ansette", "ansettelse", "anskaffe", "anskaffelse", "anslag", "anslagsvis", "anslå", "ansvarlig", "ansvarlighet", "ansvarlighetslov", "anta",
                                                             "antakelig", "antakelse", "antall", "antatt", "antyde", "antydning", "anvende", "anvendelig", "anvendelighet", "anvendelse", "arbeidsledig",
-                                                            "nevne", "nevnte", "nevnes", "nevner", "nevnt", "nevn"
+                                                            "nevne", "nevnte", "nevnes", "nevner", "nevnt", "nevn", "farlig", "farligere", "farligst", "farligste",
+                                                            "arbeidsledighet", "arbeidsmarked", "arbeidssøker", "arbeidstillatelse", "aferd", "avbrudd", "avgjørelse", "avhengighet", "avhold", "avlegge", "avsatt", "avsetning", "avsette", "avsindig", "avskrivning", "avskygning", "avspeile", "avstedkomme", "avstemning", "avstå","avstår", "avståelse", "avta", "avveie", "avveiing", "avverge",
+                                                            "barmhjertig", "bearbeide", "beboer", "bebreide", "bebygge", "bebyggelse", 
+                                                            "bedervet", "bedrøvelig", "bedømme", "bedømmelse", "befaling", "befaring", 
+                                                            "befolkning", "beføyelse", "begavet", "begeistret", "begivenhet", "begjær", 
+                                                            "begravelse","begravelsen", "begrep","begreper", "begrense", "begrune", "begrunnelse", "begynnelse","begynnelsen", 
+                                                            "begå", "behag", "beherkse", "beholde", "beholdning", 
+                                                            "bekjempe", "bekjempelse", "bekostning", "bekrefte", "belaste", "belastning",
+                                                            "beliggenhet", "belyse","belyser", "belønne", "belønning", "belønningsordning", "beløp",
+                                                            "beløpe seg til", "bemanning", "beregne","beregningen","beregning", "beregnigene", "berettiget",
+                                                            "besette", "besitte", "besittelse", "beskatte", "beskatning", "arbeidsledig", "arbeidsledige", "løs", "løse", "løst",
+                                                            "beskrive", "beskrivelse", "beskytte","beskytter","beskyttet",  
+                                                            "beskyttelse", "beslutte", "beslutning", "besparelse", "bestand", "bestemmelse", 
+                                                            "bestilling", "bestillingsfullmakt", "bestå av", "betegnelse", "betinge", "betingelse",
+                                                            "betinget", "betjene", "betrakte", "betraktning", "betryggende",
+                                                            "bety","betyr", "betydelig", "betydning", "bevege", "bevegelse", "beveggrunn", "bevilge",
+                                                            "bevilgning", "bevilling", "bevisst", "bevissthet", "bidra", "bifalle","bidrar","tegn"
+
+                                                        
+
+
+
 
 
 
@@ -42,8 +63,28 @@ public class Translation {
                                                             "politimeldinga","politimeldingar","politimeldingane","bokmeldinga","bokmeldingar","bokmeldingane",
                                                             "oppmoding", "oppmodingsvedtak", "annleis", "tilsett", "monaleg", "tilsetja", "tilsetjing", "skaffa", "anskaffing", "overslag", "rundt rekna", "rekna til", "ansvarleg", "ansvarlegheit", "ansvarlegheitslov", "gå ut frå",
                                                             "truleg", "godtaking", "tal", "pårekna", "nemna", "snev", "bruka", "brukande", "bruksverdi", "bruk", "arbeidslaus",
-                                                            "nemne", "nemnde", "nemnast", "nemner", "nemnt", "nemn"
+                                                            "nemne", "nemnde", "nemnast", "nemner", "nemnt", "nemn", "farleg", "farlegare", "farlegast", "farlegaste",
+                                                            "arbeidsløyse", "arbeidsmarknad", "arbeidssøkjar", "arbeidsløyve", "åtferd", "avbrot", "avgjerd", "avhengigheit", "fråhald", "besøkja", "avsett", "avsetjing", "setja av", "vitlaus", "avskriving", "nyanse", "spegl", "gjera", "røysting", "avhenda","avhendar", "avkall", "falla", "vega", "avveging", "hindra",
+                                                            "miskunnsam", "arbeida", "bebuar", "lasta", "byggja", "busetnad", "skjemd", "bedrøveleg",
+                                                            "vurdera", "omdomming", "påbod", "synfaring", "folksetnad", "fullmakt", "gåverik", "oppglødd",
+                                                            "hending", "begjær", "gravferd","gravferda", "omgrep","omgrep", "avgrensa", "grunnje", "grunnjeving", "byrjing","byrjinga",
+                                                            "gjera", "hugnad", "meistre", "behalda", "behaldning", 
+                                                            "motarbeide", "strid", "kostnad", "stadfeste", "belasta", "belastning",
+                                                            "plassering", "klårgjera","klårgjerar", "løna", "løn", "påskjøningsording", "sum",
+                                                            "kosta", "bemmaning", "rekna", "utrekning","utrekninga","utrekningane", "velgrunna",
+                                                            "tilsetja", "eiga", "eigedom", "skattleggja", "skattlegging", "arbeidslaus", "arbeidslause","laus", "lause", "laust",
+                                                            "forklara", "forteljing", "verna","vernar","verna", 
+                                                            "vern", "avgjera", "avgjerd", "innsparing", "populasjon", "føresegn",
+                                                            "bestilling", "bestillingsfullmakt", "bestå av", "nemning", "føresetja", "vilkår",
+                                                            "betinget", "betala", "vurdera", "syn", "forsvarleg",
+                                                            "innebera","inneberar", "monaleg", "følgje", "flytta", "rørsle", "grunn", "løyva", 
+                                                            "løyving","løyve", "medviten", "medvit", "yta", "godkjenna","ytar","teikn"
+                                                    
                                                             
+                                                            
+
+                                                            
+
 
 
                                                         
@@ -145,7 +186,7 @@ public class Translation {
 
     public static void main(String[] args) { //test kode
 
-        Translation demo = new Translation("eg!", false);
+        Translation demo = new Translation("medvit", false);
         demo.getTranslation();
 
     }
